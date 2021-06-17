959b04b87ff8944065264c09b0ebefb9234d8bb6
A601-13 <82490771+inoueyuu@users.noreply.github.com>
2021-06-17 11:09:11 +0900

    マウス

--- v0617_/Form1.cs
+++ v0617_/Form1.cs
@@ -12,10 +12,56 @@ namespace v0617_
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+        int score = 100;
         public Form1()
         {
             InitializeComponent();
         }
+        private void label2_Click(object sender, EventArgs e)
+        {
 
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Point spos = MousePosition;
+            Point fpos = PointToClient(spos);
+            label3.Left = fpos.X-label3.Width/2;
+            label3.Top = fpos.Y-label3.Height/2;
+            label3.Text = $"{fpos.X},{fpos.Y}";
+
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+            score--;
+            label2.Text = $"Score{score}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
